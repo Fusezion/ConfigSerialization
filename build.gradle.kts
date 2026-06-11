510751aa574563245c4bc9a0cf28803f2f9e5413
Fusezion <fusezionstream@gmail.com>
2026-06-10 21:54:42 -0400

    Test jitpack fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	kotlin("jvm") version "2.4.0"
 	kotlin("plugin.serialization") version "2.3.21"
 	id("com.gradleup.shadow") version "9.4.2"
+	`maven-publish`
 }
 
 repositories {
@@ -16,6 +17,10 @@ dependencies {
 
 kotlin {
 	jvmToolchain(21)
+}
+
+java {
+	withSourcesJar()
 }
 
 tasks {
