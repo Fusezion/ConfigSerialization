20e29f387002ccedb0ab7da0fdc482a51eff9c76
Fusezion <fusezionstream@gmail.com>
2026-06-13 00:37:50 -0400

    Bump version to 1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.lyric"
-version = "1.0.0"
+version = "1.1.2"
 
 repositories {
 	mavenCentral()
@@ -23,7 +23,7 @@ kotlin {
 
 java {
 	withSourcesJar()
-	withJavadocJar() // optional but recommended for IDE compatibility
+	withJavadocJar()
 }
 
 publishing {
